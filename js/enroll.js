fe22f114df4a24361fc1f54747c6af944cf979f9
xy <2155586608@qq.com>
2024-11-25 13:54:41 +0800

    完成

---
+++ js/enroll.js
@@ -0,0 +1,17 @@
+function ma() {
+    var bac=document.getElementById("logIn-background");
+    var log=document.getElementById("logIn");
+    bac.style.cssText= "visibility: visible;"
+    log.style.cssText= "visibility: visible;"
+}
+
+function mb() {
+    var bac=document.getElementById("logIn-background");
+    var log=document.getElementById("logIn");
+    bac.style.cssText= "visibility: hidden;"
+    log.style.cssText= "visibility: hidden;"
+}
+
+function mc() {
+    var bac=document.getElementById("logIn-background");
+}
