4a6dfaeaf6e286d555c0c4b051a525ae0b297db4
xy <2155586608@qq.com>
2024-11-26 08:17:42 +0800

    完成该页面

--- js/enroll.js
+++ js/enroll.js
@@ -2,16 +2,63 @@ function ma() {
     var bac=document.getElementById("logIn-background");
     var log=document.getElementById("logIn");
     bac.style.cssText= "visibility: visible;"
-    log.style.cssText= "visibility: visible;"
+    log.style.cssText= "visibility: visible;transform: translateY(0px);"
 }
 
 function mb() {
     var bac=document.getElementById("logIn-background");
     var log=document.getElementById("logIn");
+    var ter=document.getElementById("logUp");
     bac.style.cssText= "visibility: hidden;"
     log.style.cssText= "visibility: hidden;"
+    ter.style.cssText= "visibility: hidden;transform: translateY(100px);"
 }
 
 function mc() {
+    var bac=document.getElementById("logUp");
+    var log=document.getElementById("logIn");
+    bac.style.cssText= "visibility: visible;transform: translateY(0px);"
+    log.style.cssText= "visibility: hidden;"
+}
+
+function md() {
+    var bac=document.getElementById("logUp");
+    var log=document.getElementById("logIn");
+    bac.style.cssText= "visibility: hidden;transform: translateY(100px);"
+    log.style.cssText= "visibility: visible;transform: translateY(0px);"
+}
+
+function od() {
+    var bac=document.getElementById("logUp");
+    var log=document.getElementById("logIn-background");
+    bac.style.cssText= "visibility: hidden;transform: translateY(100px);"
+    log.style.cssText= "visibility: hidden;;"
+}
+
+function me() {
+    var bac=document.getElementById("logIn3");
+    var log=document.getElementById("logIn");
+    bac.style.cssText= "visibility: visible;transform: translateY(0px);"
+    log.style.cssText= "visibility: hidden;"
+}
+
+function mf() {
+    var bac=document.getElementById("logIn3");
+    var log=document.getElementById("logIn");
+    bac.style.cssText= "visibility: hidden;transform: translateY(100px);"
+    log.style.cssText= "visibility: visible;transform: translateY(0px)"
+}
+
+function mg() {
+    var bac=document.getElementById("logUp");
+    var log=document.getElementById("logIn3");
+    bac.style.cssText= "visibility: visible;transform: translateY(0px);"
+    log.style.cssText= "visibility: hidden;transform: translateY(100px);"
+}
+
+function mk() {
     var bac=document.getElementById("logIn-background");
+    var log=document.getElementById("logIn3");
+    bac.style.cssText= "visibility: hidden;"
+    log.style.cssText= "visibility: hidden;transform: translateY(100px);"
 }
